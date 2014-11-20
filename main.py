c247c4371b619ab419e32e9db76f9575580c14c7
serdyuk <serdyuk.dmitriy@gmail.com>
2014-11-20 13:38:26 -0500

    Fixed value of q

--- main.py
+++ main.py
@@ -18,29 +18,31 @@ def main(filename, q_value, prob_x1):
     message_x_y = np.zeros((n, 2))
 
     # Probability x_{t + 1} given x_t
-    prob_x_x = np.array([[0.8, 0.2], [0.2, 0.8]])
+    prob_x_x = np.array([[0.8, 0.2],
+                         [0.2, 0.8]])
     # Probability x_t given y_t
-    prob_x_y = np.array([[q_value, (1 - q_value)], [(1 - q_value), q_value]])
+    prob_x_y = np.array([[q_value, (1 - q_value)],
+                         [(1 - q_value), q_value]])
     # Probability x_1
     prob_x1 = np.array([1 - prob_x1, prob_x1])
 
     if data[n - 1] == -1:
-        message_y_x[n - 1, 0] = 1. - q_value
-        message_y_x[n - 1, 1] = q_value
-    else:
         message_y_x[n - 1, 0] = q_value
         message_y_x[n - 1, 1] = 1. - q_value
+    else:
+        message_y_x[n - 1, 0] = 1. - q_value
+        message_y_x[n - 1, 1] = q_value
 
     message_x_back[n - 1, :] = (message_y_x[n - 1, 0] * prob_x_x[0, :] +
                                 message_y_x[n - 1, 1] * prob_x_x[1, :])
 
     for i in reversed(xrange(0, n - 1)):
         if data[i] == -1:
-            message_y_x[i, 0] = 1 - q_value
-            message_y_x[i, 1] = q_value
-        else:
             message_y_x[i, 0] = q_value
-            message_y_x[i, 1] = 1 - q_value
+            message_y_x[i, 1] = 1. - q_value
+        else:
+            message_y_x[i, 0] = 1. - q_value
+            message_y_x[i, 1] = q_value
 
         message_x_back[i, :] = (message_y_x[i, 0] * message_x_back[i + 1, 0] * prob_x_x[0, :] +
                                 message_y_x[i, 1] * message_x_back[i + 1, 1] * prob_x_x[1, :])
@@ -59,10 +61,10 @@ def main(filename, q_value, prob_x1):
     message_x_fort[n - 1, :] = (prob_x_x[:, 1] * message_y_x[n - 1, 1] * message_x_fort[n - 2, 1] +
                                 prob_x_x[:, 0] * message_y_x[n - 1, 0] * message_x_fort[n - 2, 0])
 
-    #message_y_x /= np.sum(message_y_x, axis=1).reshape((-1, 1))
-    #message_x_fort /= np.sum(message_x_fort, axis=1).reshape((-1, 1))
-    #message_x_back /= np.sum(message_x_back, axis=1).reshape((-1, 1))
-    #message_x_y /= np.sum(message_x_y, axis=1).reshape((-1, 1))
+    message_y_x /= np.sum(message_y_x, axis=1).reshape((-1, 1))
+    message_x_fort /= np.sum(message_x_fort, axis=1).reshape((-1, 1))
+    message_x_back /= np.sum(message_x_back, axis=1).reshape((-1, 1))
+    message_x_y /= np.sum(message_x_y, axis=1).reshape((-1, 1))
     return message_y_x, message_x_fort, message_x_back, message_x_y
 
 
